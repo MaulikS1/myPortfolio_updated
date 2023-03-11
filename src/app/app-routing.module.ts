ef87eb0de3b22730c52f3bcce440af07924de918
MaulikS1 <mauliksolanki48@gmail.com>
2023-03-12 02:04:57 +0530

    code updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { EducationComponent } from './education/education.component';
-import { AchievementsComponent } from './achievements/achievements.component';
 import { ContactComponent } from './contact/contact.component';
+import { AchievementsComponent } from './achievements/achievements.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'education', component: EducationComponent },
-  { path: 'achievements', component: AchievementsComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'achievements', component: AchievementsComponent }
 ];
 
 @NgModule({
